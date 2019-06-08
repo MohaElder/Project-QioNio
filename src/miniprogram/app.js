9b50933e49b1d2dcdfb7e5120e6a3c16a8a7795d
MohaElder169 <calen0909@hotmail.com>
2019-06-08 10:51:39 +0800

    init go cloud

--- src/miniprogram/app.js
+++ src/miniprogram/app.js
@@ -12,6 +12,14 @@ App({
         this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
       }
     })
+
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      })
+    }
   },
   globalData: {
     userInfo: {},
--- src/miniprogram/pages/index/index.js
+++ src/miniprogram/pages/index/index.js
@@ -1,6 +1,8 @@
 //index.js
 const app = getApp()
+const db = wx.cloud.database();
 
+//const userSearcher = db.collection('user');
 Page({
   data: {
     user: {},
@@ -22,23 +24,58 @@ Page({
   },
 
   onLoad: function() {
-    var that = this;
-    // 获取用户信息
-    wx.getUserInfo({
+    this.onGetOpenid()
+    this.upload();
+  },
+
+  upload: function () {
+    console.log("Running Upload")
+    console.log("openid inside Upload:" + app.globalData.openid)
+    //var Time = util.formatTime(new Date());
+    db.collection('user').doc(app.globalData.openid).get({//建立或者更新数据库信息
       success: function (res) {
-        var userInfo = res.userInfo;
-        var user = {info:userInfo,orderID:""};
+        console.log("Found Match")
+        app.globalData.user = res.data.user;
+        console.log("Completed!")
+        var that = this;
         that.setData({
-          user: user
+          orderList: app.globalData.orderList,
+          user: app.globalData.user
+        })
+        // res.data 包含该记录的数据
+        wx.showModal({
+          title: 'Status',
+          content: 'Login Complete!',
         });
-        app.globalData.user = user;
-        console.log(app.globalData.user)
       },
-      fail: function (res) {
+      fail: function () {
+        console.log("No Match")
+        wx.getUserInfo({
+          success: function (res){
+            var userInfo = res.userInfo;
+            var user = { info: userInfo, orderID:[]};
+            app.globalData.user = user;
+            db.collection('user').add({
+              data: {
+                _id:openid,
+                user: app.globalData.user
+              }
+            })
+            that.setData({
+              orderList: app.globalData.orderList,
+              user: app.globalData.user
+            })            
+            console.log(app.globalData.user)
+            wx.showModal({
+              title: 'Status',
+              content: 'Your profile has been created!',
+            });
+          },
+          fail: function (res) {
+          }
+        })
+        // 获取用户信息
       }
-    })
-    that.setData({
-      orderList: app.globalData.orderList
     })
   },
 
@@ -56,6 +93,28 @@ Page({
     wx.navigateTo({
       url: '../self/self',
     })
-  }
+  },
+
+  onGetOpenid: function () {
+    console.log("Getting Openid......")
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        //console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        //console.log(res.result.openid)
+        console.log("Got openID! openID: " + app.globalData.openid);
+      },
+      fail: err => {
+        //console.error('[云函数] [login] 调用失败', err)
+        wx.showModal({
+          title: '警告',
+          content: '出了点问题',
+        })
+      }
+    })
+  },
 
 })
