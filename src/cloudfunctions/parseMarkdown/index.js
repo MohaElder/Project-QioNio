c407af14a926cf1140eb1d110c3716418e7f8aa1
MohaElder169 <calen0909@hotmail.com>
2019-07-13 20:27:39 +0800

    clean up cloud functions

--- src/cloudfunctions/parseMarkdown/index.js
+++
@@ -1,8 +0,0 @@
-const parser = require('parser-wxapp');
-var html = "<div>Hello World!</div>";
-parser(html).then(res => {
-  return res;
-  console.log(res);
-}).catch(err => {
-  console.error(err);
-})
